1b49063666823f120136e2125ff4ab27a646650b
Josh Simmons <josh.simmons@gmail.com>
2016-03-10 21:21:01 -0800

    Adding shift register.

--- program.c
+++ program.c
@@ -34,12 +34,40 @@ void wait1() {
   }
 }
 
+void init_shift() {
+  // PA0 is clock, PD5 is data
+  DDRA |= _BV(DDRA0);
+  DDRD |= _BV(DDD5);
+}
+
+void shift(char byte) {
+  int bit;
+  for (bit = 0; bit < 8; ++bit) {
+    PORTA &= ~_BV(PORTA0);
+    if (byte & 0x80) {
+      PORTD |= _BV(PORTD5);
+    } else {
+      PORTD &= ~_BV(PORTD5);
+    }
+    PORTA |= _BV(PORTA0);
+    byte <<= 1;
+  }
+  // ♪♫ One last shift, just like the good old times. ♪♫
+  // Clocking shifts the /last/ state of the shift register into the output
+  // register. We'll need to actually shift a bit here if we want cleverness.
+  PORTA &= ~_BV(PORTA0);
+  PORTA |= _BV(PORTA0);
+}
+
 int main(void) {
   usart_init();
+  init_shift();
+  // Set up the LED
   DDRB |= _BV(DDB0);
   PORTB |= _BV(PB0);
   while (1) {
     char c = usart_read();
+    shift(c);
     if (c & 1) {
       PORTB &= ~_BV(PB0);
     } else {
